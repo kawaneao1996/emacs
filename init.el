93726ad8e1de0aca2c984a21f4944f76be606cba
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2023-02-23 17:24:59 +0900

    private.elを作る必要性が無くなった。

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
-(org-babel-load-file
- (expand-file-name
-  "private.org"
-  user-emacs-directory)
- )
+;; (org-babel-load-file
+;;  (expand-file-name
+;;   "private.org"
+;;   user-emacs-directory)
+;;  )
 (org-babel-load-file
  (expand-file-name
   "leaf_config.org"
